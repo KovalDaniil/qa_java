3cea1684fea0ed66398ee33ddbde528fdc0ac3e9
KovalDaniil <danielforework@mail.ru>
2026-09-09 15:36:13 +0300

    поправил конструктор и параметризованные тесты

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -7,8 +7,15 @@ public class Lion {
     boolean hasMane;
     private final Feline feline;
 
-    public Lion(Feline feline) {
+    public Lion(String sex, Feline feline) throws Exception {
         this.feline = feline;
+        if ("Самец".equals(sex)) {
+            hasMane = true;
+        } else if ("Самка".equals(sex)) {
+            hasMane = false;
+        } else {
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
+        }
     }
 
     public int getKittens() {
--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -19,21 +19,41 @@ public class LionTest {
     @Mock
     private Feline feline;
 
+//    @Test
+//    public void testLionWithMaleHasMane() throws Exception {
+//        Lion lion = new Lion("Самец", feline);
+//
+//        assertTrue(lion.doesHaveMane());
+//    }
+//
+//    @Test
+//    public void testLionWithFemaleNoMane() throws Exception {
+//        Lion lion = new Lion("Самка", feline);
+//
+//        assertFalse(lion.doesHaveMane());
+//    }
+
     @ParameterizedTest
     @CsvSource({
             "Самец, true",
             "Самка, false"
     })
-    public void testLionSexAndManeRelationship(boolean expectedHasMane) {
-        Lion lion = new Lion(feline);
+    public void testLionSexAndManeRelationship(String sex, boolean expectedHasMane) throws Exception {
+        Lion lion = new Lion(sex, feline);
+
         assertEquals(expectedHasMane, lion.doesHaveMane());
     }
 
     @Test
-    public void testGetKittensDelegatesToFeline() {
+    public void testLionInvalidSexThrowsException() {
+        assertThrows(Exception.class, () -> new Lion("Неизвестный", feline));
+    }
+
+    @Test
+    public void testGetKittensDelegatesToFeline() throws Exception {
         when(feline.getKittens()).thenReturn(3);
 
-        Lion lion = new Lion(feline);
+        Lion lion = new Lion("Самец", feline);
         int kittens = lion.getKittens();
 
         verify(feline, times(1)).getKittens();
@@ -42,10 +62,10 @@ public class LionTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 5, 10})
-    public void testGetKittensWithVariousCounts(int expectedKittens) {
+    public void testGetKittensWithVariousCounts(int expectedKittens) throws Exception {
         when(feline.getKittens()).thenReturn(expectedKittens);
 
-        Lion lion = new Lion(feline);
+        Lion lion = new Lion("Самец", feline);
         int actualKittens = lion.getKittens();
 
         verify(feline, times(1)).getKittens();
@@ -57,17 +77,10 @@ public class LionTest {
         List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
         when(feline.eatMeat()).thenReturn(expectedFood);
 
-        Lion lion = new Lion(feline);
+        Lion lion = new Lion("Самка", feline);
         List<String> actualFood = lion.getFood();
 
         verify(feline, times(1)).eatMeat();
         assertEquals(expectedFood, actualFood);
-    }
-
-    @Test
-    public void testLionConstructorWithoutSexDoesNotSetMane() {
-        Lion lion = new Lion(feline);
-
-        assertFalse(lion.doesHaveMane());
     }
 }
